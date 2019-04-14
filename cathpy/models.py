96a3f87732bf94ec674fe8bc2befe52640a2ef66
sillitoe <ian@sillit.com>
2019-04-14 17:11:50 +0100

    move AminoAcid and AminoAcids to models

--- cathpy/models.py
+++ cathpy/models.py
@@ -1,4 +1,93 @@
-"""CATH Models used"""
+"""
+Collection of classes used to model CATH data
+"""
+
+class AminoAcid(object):
+    """Class representing an Amino Acid."""
+
+    def __init__(self, one, three, word):
+        self.one = one
+        self.three = three
+        self.word = word
+
+    def __str__(self):
+        """Return this AminoAcid as a single character string"""
+        return self.one
+
+class AminoAcids(object):
+    """Provides access to recognised Amino Acids."""
+
+    aa_by_id = {aa[2]: AminoAcid(aa[2], aa[1], aa[0]) for aa in (
+        ('alanine', 'ala', 'A'),
+        ('arginine', 'arg', 'R'),
+        ('asparagine', 'asn', 'N'),
+        ('aspartic acid', 'asp', 'D'),
+        ('asparagine', 'asx', 'B'),
+        ('cysteine', 'cys', 'C'),
+        ('glutamic acid', 'glu', 'E'),
+        ('glutamine', 'gln', 'Q'),
+        ('glutamic acid', 'glx', 'Z'),
+        ('glycine', 'gly', 'G'),
+        ('histidine', 'his', 'H'),
+        ('isoleucine', 'ile', 'I'),
+        ('leucine', 'leu', 'L'),
+        ('lysine', 'lys', 'K'),
+        ('methionine', 'met', 'M'),
+        ('phenylalanine', 'phe', 'F'),
+        ('proline', 'pro', 'P'),
+        ('serine', 'ser', 'S'),
+        ('threonine', 'thr', 'T'),
+        ('tryptophan', 'trp', 'W'),
+        ('tyrosine', 'tyr', 'Y'),
+        ('valine', 'val', 'V'),
+    )}
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def is_valid_aa(cls, aa_letter):
+        """Check if aa is a valid single character aa code."""
+        return str(aa_letter).upper() in cls.aa_by_id
+
+    @classmethod
+    def get_by_id(cls, aa_letter):
+        """Return the AminoAcid object by the given single character aa code."""
+        return cls.aa_by_id[aa_letter.upper()]
+
+class Residue(object):
+    """Class to represent a protein residue."""
+
+    def __init__(self, aa, seq_num=None, pdb_label=None, *, pdb_aa=None):
+        assert isinstance(aa, str)
+        if seq_num:
+            seq_num = int(seq_num)
+
+        self.aa = aa
+        self.seq_num = seq_num
+        self.pdb_label = pdb_label
+        self.pdb_aa = pdb_aa if pdb_aa else aa
+
+    def __str__(self):
+        return self.aa
+
+    def __repr__(self):
+        return "res({},seq:{},pdb:{},pdb_aa:{})".format(
+            self.aa, self.seq_num, self.pdb_label, self.pdb_aa)
+
+class Segment(object):
+    """Class to represent a protein segment."""
+
+    def __init__(self, start: int, stop: int):
+        self.start = int(start)
+        self.stop = int(stop)
+
+    def __str__(self):
+        return "{}-{}".format(self.start, self.stop)
+
+    def __repr__(self):
+        return "Segment:{}-{}".format(self.start, self.stop)
+
 
 class ScanHsp(object):
     """Object to store the High Scoring Pair (HSP) from a sequence scan."""
--- cathpy/seqio.py
+++ cathpy/seqio.py
@@ -10,94 +10,10 @@ import functools
 
 # local
 from cathpy import error as err, util
+from cathpy.models import AminoAcid, AminoAcids, Residue, Segment
+
 
 LOG = logging.getLogger(__name__)
-
-class AminoAcid(object):
-    """Class representing an Amino Acid."""
-
-    def __init__(self, one, three, word):
-        self.one = one
-        self.three = three
-        self.word = word
-
-    def __str__(self):
-        """Return this AminoAcid as a single character string"""
-        return self.one
-
-class AminoAcids(object):
-    """Provides access to recognised Amino Acids."""
-
-    aa_by_id = {aa[2]: AminoAcid(aa[2], aa[1], aa[0]) for aa in (
-        ('alanine', 'ala', 'A'),
-        ('arginine', 'arg', 'R'),
-        ('asparagine', 'asn', 'N'),
-        ('aspartic acid', 'asp', 'D'),
-        ('asparagine', 'asx', 'B'),
-        ('cysteine', 'cys', 'C'),
-        ('glutamic acid', 'glu', 'E'),
-        ('glutamine', 'gln', 'Q'),
-        ('glutamic acid', 'glx', 'Z'),
-        ('glycine', 'gly', 'G'),
-        ('histidine', 'his', 'H'),
-        ('isoleucine', 'ile', 'I'),
-        ('leucine', 'leu', 'L'),
-        ('lysine', 'lys', 'K'),
-        ('methionine', 'met', 'M'),
-        ('phenylalanine', 'phe', 'F'),
-        ('proline', 'pro', 'P'),
-        ('serine', 'ser', 'S'),
-        ('threonine', 'thr', 'T'),
-        ('tryptophan', 'trp', 'W'),
-        ('tyrosine', 'tyr', 'Y'),
-        ('valine', 'val', 'V'),
-    )}
-
-    def __init__(self):
-        pass
-
-    @classmethod
-    def is_valid_aa(cls, aa_letter):
-        """Check if aa is a valid single character aa code."""
-        return str(aa_letter).upper() in cls.aa_by_id
-
-    @classmethod
-    def get_by_id(cls, aa_letter):
-        """Return the AminoAcid object by the given single character aa code."""
-        return cls.aa_by_id[aa_letter.upper()]
-
-class Residue(object):
-    """Class to represent a protein residue."""
-
-    def __init__(self, aa, seq_num=None, pdb_label=None, *, pdb_aa=None):
-        assert isinstance(aa, str)
-        if seq_num:
-            seq_num = int(seq_num)
-
-        self.aa = aa
-        self.seq_num = seq_num
-        self.pdb_label = pdb_label
-        self.pdb_aa = pdb_aa if pdb_aa else aa
-
-    def __str__(self):
-        return self.aa
-
-    def __repr__(self):
-        return "res({},seq:{},pdb:{},pdb_aa:{})".format(
-            self.aa, self.seq_num, self.pdb_label, self.pdb_aa)
-
-class Segment(object):
-    """Class to represent a protein segment."""
-
-    def __init__(self, start: int, stop: int):
-        self.start = int(start)
-        self.stop = int(stop)
-
-    def __str__(self):
-        return "{}-{}".format(self.start, self.stop)
-
-    def __repr__(self):
-        return "Segment:{}-{}".format(self.start, self.stop)
 
 class Sequence(object):
     """Class to represent a protein sequence."""
@@ -469,6 +385,15 @@ class Correspondence(object):
     A correspondence is a type of alignment that provides the equivalences
     between the residues in the protein sequence (eg ``SEQRES`` records) and
     the residues actually observed in the structure (eg ``ATOM`` records).
+
+    Within CATH, this is most commonly initialised from a GCF file:
+
+    ```
+    aln = Correspondence.new_from_gcf('/path/to/<id>.gcf')
+    ```
+
+    TODO: allow this to be created from PDBe API endpoint.
+
     """
 
     GCF_GAP_CHAR = '*'
