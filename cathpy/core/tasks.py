24e784b7189ade642eb7f34ab803c356b5e8cf3a
isillitoe <ian@sillit.com>
2019-11-27 17:35:44 +0000

    add celery task to process ssaps

---
+++ cathpy/core/tasks.py
@@ -0,0 +1,56 @@
+import logging
+
+from cathpy.core import models, ssap
+from celery import Celery
+import redis
+
+DEFAULT_REDIS_HOST = 'localhost'
+DEFAULT_REDIS_DB = '1'
+DEFAULT_REDIS_PORT = 6379
+DEFAULT_PDB_PATH = '/cath/data/current/pdb'
+
+DEFAULT_CELERY_BROKER = 'redis://localhost'
+DEFAULT_CELERY_BACKEND = 'redis://localhost'
+
+app = Celery('cathpy.core.tasks',
+             backend=DEFAULT_CELERY_BACKEND,
+             broker=DEFAULT_CELERY_BROKER)
+
+
+LOG = logging.getLogger(__name__)
+
+
+@app.task
+def run_ssap(id1, id2, *, cath_version, pdb_path=DEFAULT_PDB_PATH, datastores=None):
+
+    ssap_runner = ssap.SsapRunner(
+        pdb_path=pdb_path, cath_version=cath_version)
+
+    ssap_runner.run(id1, id2, datastores=datastores)
+
+
+@app.task
+def run_ssap_pairs(pairs, *, cath_version, pdb_path=DEFAULT_PDB_PATH, datastores=None, max_consecutive_errors=5):
+
+    ssap_runner = ssap.SsapRunner(
+        pdb_path=pdb_path, cath_version=cath_version)
+
+    if datastores:
+        datastores = [ssap.SsapStorageFactory.get(d) for d in datastores]
+
+    consecutive_error_count = 0
+
+    for idx, pair in enumerate(pairs):
+        id1, id2 = pair
+        try:
+            ssap_runner.run(id1, id2, datastores=datastores)
+            consecutive_error_count = 0
+        except Exception as e:
+            consecutive_error_count += 1
+            LOG.error(
+                f"Caught exception when running ssap {id1} {id2} (skipping) (err:{e})")
+
+        if consecutive_error_count > max_consecutive_errors:
+            msg = f"Encounted max consecutive errors ({consecutive_error_count}) - bailing out"
+            LOG.error(msg)
+            raise Exception(msg)
