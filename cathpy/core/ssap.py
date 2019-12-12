18aece49718e47986f5708ad26fcae80b4c12f47
isillitoe <ian@sillit.com>
2019-12-12 15:19:31 +0000

    add local cath-ssap exe

--- cathpy/core/ssap.py
+++ cathpy/core/ssap.py
@@ -2,6 +2,8 @@ import json
 import logging
 import os
 import re
+import pkg_resources
+import platform
 import subprocess
 import tempfile
 from urllib.parse import urlparse
@@ -12,15 +14,20 @@ LOG = logging.getLogger()
 
 DEFAULT_CHUNK_SIZE = 1000
 
+TOOL_DIR = pkg_resources.resource_filename(__name__, "../tools")
+PLATFORM_DIRNAME = "{}-{}".format(platform.system().lower(),
+                                  platform.machine())
+CATH_SSAP_EXE = os.path.join(TOOL_DIR, PLATFORM_DIRNAME, 'cath-ssap')
+
 
 class SsapBatch:
     """
     Provides management functions for performing a batch of SSAPs
     """
 
-    def __init__(self, *, pairs_file, datastore, cath_version):
+    def __init__(self, *, pairs_file, datastore_dsn, cath_version):
         self.pairs_file = pairs_file
-        self.datastore = SsapStorageFactory.get(datastore)
+        self.datastore = SsapStorageFactory.get(datastore_dsn)
         self.cath_version = version.CathVersion(cath_version)
         self._pairs_fh = None
         self._pairs_line_count = None
@@ -32,6 +39,8 @@ class SsapBatch:
 
         processed = 0
         pairs_missing = []
+
+        report_chunk = 10000
 
         with open(self.pairs_file) as fh:
             for line_number, line in enumerate(fh, 1):
@@ -47,6 +56,9 @@ class SsapBatch:
 
                 # LOG.debug("[{:3d}] id1:{} id2:{}  [{}]".format(
                 #     line_number, id1, id2, found_ssap))
+
+                if line_number % report_chunk == 0:
+                    LOG.debug(f"  ... processed {line_number} SSAP pairs")
 
                 if not found_ssap or force:
                     pairs_missing.extend([[id1, id2]])
@@ -137,42 +149,8 @@ class SsapResult:
         d = self.__dict__
         d['simax_score'] = self.simax_score
         if ignore_empty:
-            d = {k: v for k, v in d.items() if v}
+            d = {k: v for k, v in d.items() if v is not None}
         return d
-
-    def to_redis(self, redis, key=None):
-        """
-        Sets the SSAP result in the provided Redis datastore
-
-        Args:
-            redis (Redis): redis connection
-        """
-        if not key:
-            key = self.unique_key()
-        v = self.to_dict(ignore_empty=True)
-        j = json.dumps(v)
-        # LOG.info(f'Setting redis key {key}={v[:10]}...')
-        try:
-            redis.set(key, j)
-        except:
-            LOG.error(f"Encountered error when setting redis key '{key}'")
-            raise
-
-    @classmethod
-    def from_redis(cls, redis, id1, id2, cath_version):
-        """
-        Attempts to create a SSAP result from the provided Redis datastore
-
-        Args:
-            redis (Redis): redis connection
-        """
-        key = cls.mk_key(cath_version=cath_version, id1=id1, id2=id2)
-        j = redis.get(key)
-        ssap_args = json.loads(j)
-        LOG.info(f"Creating SsapResult from redis data={ssap_args}")
-        del ssap_args['simax_score']
-        ssap_result = cls(**ssap_args)
-        return ssap_result
 
 
 class SsapRunner:
@@ -185,28 +163,26 @@ class SsapRunner:
     * pdb_path (str): path to find PDB files
     """
 
-    def __init__(self, *, ssap_exe='cath-ssap', pdb_path=None, cath_version):
+    def __init__(self, *, ssap_exe=CATH_SSAP_EXE, pdb_path=None, cath_version):
         self.ssap_exe = ssap_exe
         self.pdb_path = pdb_path
         self.cath_version = cath_version
         self.tmp_aln_dir = tempfile.TemporaryDirectory()
 
-    def run(self, id1, id2, *, lock=None, datastores=None):
+    def run(self, id1, id2, *, lock=None, datastore_dsn=None):
         cath_version = self.cath_version
 
         ssap_key = SsapResult.mk_key(
             cath_version=cath_version, id1=id1, id2=id2)
 
-        redis_stores = [
-            ds for ds in datastores if isinstance(ds, RedisSsapStore)]
-        if redis_stores:
-            redis_ds = SsapStorageFactory.get(redis_stores[0])
+        if datastore_dsn:
+            ds = SsapStorageFactory.get(datastore_dsn)
             ssap_result = None
-            if ssap_key in redis_ds:
-                LOG.info(f"Found SSAP: [{ssap_key}]")
+            if ssap_key in ds:
+                LOG.debug(f"Found SSAP: [{ssap_key}]")
                 try:
-                    ssap_result = SsapResult.from_redis(
-                        redis=redis_ds.redis, id1=id1, id2=id2, cath_version=cath_version)
+                    ssap_result = ds.get_ssap_result(
+                        id1=id1, id2=id2, cath_version=cath_version)
                 except Exception as e:
                     LOG.warning(
                         f"Failed to create SsapResult from existing data in redis ({e}), running again...")
@@ -237,19 +213,18 @@ class SsapRunner:
                 aln_text = aln_fh.read()
             ssap_result.alignment_text = aln_text
 
-            if datastores:
-                for datastore in datastores:
-                    try:
-                        ds = SsapStorageFactory.get(datastore)
-                    except TypeError as err:
-                        LOG.error(
-                            "Failed to get ssap datastore with definition '{datastore}'")
-                        raise
+            if datastore_dsn:
+                try:
+                    ds = SsapStorageFactory.get(datastore_dsn)
+                except TypeError as err:
+                    LOG.error(
+                        "Failed to get ssap datastore with definition '{datastore_dsn}'")
+                    raise
 
-                    LOG.info(
-                        f'Saving SSAP results [{ds.__class__.__name__}:{ssap_result.unique_key()}]')
-                    ds.set_ssap_result(
-                        ssap_result, lock=lock, alnfile=aln_file)
+                LOG.info(
+                    f'Saving SSAP results [{ds.__class__.__name__}:{ssap_result.unique_key()}]')
+                ds.set_ssap_result(
+                    ssap_result, lock=lock, alnfile=aln_file)
 
         except subprocess.CalledProcessError as error:
             LOG.error(("caught exception when running 'cath-ssap':\n",
@@ -366,16 +341,135 @@ class RedisSsapStore(SsapStore):
     def redis(self):
         return self._redis
 
-    def set_ssap_result(self, ssap_result, *args, **kwargs):
-        ssap_result.to_redis(self.redis)
+    def set_ssap_result(self, ssap_result, *, key=None, **kwargs):
+        if not key:
+            key = ssap_result.unique_key()
+        v = ssap_result.to_dict(ignore_empty=True)
+        j = json.dumps(v)
+        # LOG.info(f'Setting redis key {key}={v[:10]}...')
+        try:
+            self.redis.set(key, j)
+        except:
+            LOG.error(f"Encountered error when setting redis key '{key}'")
+            raise
+
+    def get_ssap_result(self, *, id1=None, id2=None, cath_version=None, key=None):
+        """
+        Get a SsapResult from the Redis datastore
+
+        Args:
+            id1 (str): id1
+            id2 (str): id2
+            cath_version (str): cath_version
+        """
+
+        if key and (id1 or id2 or cath_version):
+            raise Exception(
+                f"usage error: must use 'key' or ('id1', 'id2', 'cath_version')")
+
+        if not key and not (id1 and id2 and cath_version):
+            raise Exception(
+                f"usage error: must use 'key' or ('id1', 'id2', 'cath_version')")
+
+        if not key:
+            key = SsapResult.mk_key(
+                cath_version=cath_version, id1=id1, id2=id2)
+
+        j = self._redis.get(key)
+        ssap_args = json.loads(j)
+        # LOG.debug(f"Creating SsapResult from redis data={ssap_args}")
+        del ssap_args['simax_score']
+        ssap_result = SsapResult(**ssap_args)
+        return ssap_result
 
     def __contains__(self, key):
         key_exists = self._redis.exists(key)
-#        LOG.debug(f'redis.exists[{key}] {key_exists}')
+        # LOG.debug(f'redis.exists[{key}] {key_exists}')
         return key_exists
 
     def __str__(self):
         return f'redis://{self.host}:{self.port}/{self.db}'
+
+
+class MongoSsapStore(SsapStore):
+    def __init__(self, url=None, *, db='cath', host='localhost', port=27017, collection='ssaps'):
+        mongo_args = {'host': host, 'port': port}
+        if url:
+            u = urlparse(url)
+            if u.hostname:
+                mongo_args['host'] = u.hostname
+            if u.port:
+                mongo_args['port'] = u.port
+            if u.path:
+                db = u.path.replace('/', '')
+
+        self.host = mongo_args['host']
+        self.port = mongo_args['port']
+        self.db = db
+
+        LOG.debug(f"Creating Mongo connection: {mongo_args}")
+        pymongo = __import__('pymongo')
+        self._client = pymongo.MongoClient(**mongo_args)
+        self._mongodb = self._client[self.db]
+        self._collection = self._mongodb[collection]
+
+    @property
+    def mongo(self):
+        return self._client
+
+    def set_ssap_result(self, ssap_result, *, key=None, **kwargs):
+        if not key:
+            key = ssap_result.unique_key()
+        ssap_data = ssap_result.to_dict(ignore_empty=True)
+
+        # LOG.info(f'Setting mongo key {key}={v[:10]}...')
+        try:
+            self._collection.replace_one({'_id': key}, ssap_data, upsert=True)
+        except Exception as err:
+            LOG.error(
+                f"Encountered error when updating mongo document '{key}' (err: {err})")
+            raise
+
+    def get_ssap_result(self, *, id1=None, id2=None, cath_version=None, key=None):
+        """
+        Get a SsapResult from the MongoDB datastore
+
+        Args:
+            key (str): key
+            id1 (str): id1
+            id2 (str): id2
+            cath_version (str): cath_version
+
+        Returns:
+            SsapResult
+        """
+
+        if key and (id1 or id2 or cath_version):
+            raise Exception(
+                f"usage error: must use 'key' or ('id1', 'id2', 'cath_version')")
+
+        if not key and not (id1 and id2 and cath_version):
+            raise Exception(
+                f"usage error: must use 'key' or ('id1', 'id2', 'cath_version')")
+
+        if not key:
+            key = SsapResult.mk_key(
+                cath_version=cath_version, id1=id1, id2=id2)
+
+        ssap_data = self._collection.find_one({'_id': key})
+        # LOG.debug(f"Creating SsapResult from mongo data={ssap_args}")
+        del ssap_data['simax_score']
+        del ssap_data['_id']
+        ssap_result = SsapResult(**ssap_data)
+        return ssap_result
+
+    def __contains__(self, key):
+        key_exists = self._collection.find_one(key)
+        # LOG.debug(f'mongodb.exists[{key}] {key_exists}')
+        return key_exists
+
+    def __str__(self):
+        return f'mongodb://{self.host}:{self.port}/{self.db}'
 
 
 class SsapStorageFactory:
@@ -384,6 +478,7 @@ class SsapStorageFactory:
         'file': lambda arg_str: FileSsapStore(arg_str),
         'tar': lambda arg_str: TarSsapStore(arg_str),
         'redis': lambda arg_str: RedisSsapStore(arg_str),
+        'mongodb': lambda arg_str: MongoSsapStore(arg_str),
         #            **{k: v for kv in arg_str.split(';') for k, v in kv.split('=')}),
     }
 
