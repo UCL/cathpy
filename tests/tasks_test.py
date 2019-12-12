673e1519ed2adc435cfeddacaaa16a1b3f523d06
isillitoe <ian@sillit.com>
2019-12-12 15:25:40 +0000

    add mongo tests

--- tests/tasks_test.py
+++ tests/tasks_test.py
@@ -3,15 +3,18 @@ from os import path
 import tempfile
 
 import pytest
-import redis
 from cathpy.core import ssap, tasks
 from celery import Celery
 from . import testutils
+
+import redis
+import pymongo
 
 LOG = logging.getLogger(__name__)
 
 
 REDIS_TEST_DATABASE = 3
+MONGO_TEST_DATABASE = 'test'
 
 
 def redis_is_available():
@@ -20,10 +23,23 @@ def redis_is_available():
         redis.Redis(db=REDIS_TEST_DATABASE).ping()
         return True
     except redis.RedisError:
+        LOG.info('Failed to connect to redis instance (skipping tests ...)')
+        return False
+
+
+def mongo_is_available():
+    LOG.info('Testing mongodb connection...')
+    try:
+        pymongo.MongoClient()
+        return True
+    except Exception as err:
+        LOG.info(
+            f'Failed to connect to mongo instance (ERR:{err}) (skipping tests ...)')
         return False
 
 
 @pytest.mark.skipif(not redis_is_available(), reason="Failed to connect to local redis instance (skipping tests)")
+@pytest.mark.skipif(not mongo_is_available(), reason="Failed to connect to local mongo instance (skipping tests)")
 class TestSsap(testutils.TestBase):
 
     def setUp(self):
@@ -40,6 +56,8 @@ class TestSsap(testutils.TestBase):
         self.pdb_path = path.join(self.test_data_dir, 'pdb')
         self.redis_ds = ssap.SsapStorageFactory.get(
             'redis://localhost', db=REDIS_TEST_DATABASE)
+        self.mongo_ds = ssap.SsapStorageFactory.get(
+            'mongodb://localhost', db=MONGO_TEST_DATABASE)
 
         pairs = []
         with open(self.ssap_pairs_file) as fh:
@@ -56,22 +74,48 @@ class TestSsap(testutils.TestBase):
         ds.redis.delete(key)
         self.assertFalse(ds.redis.get(key),
                          f'redis key {key} is empty')
-        ssap_res.to_redis(ds.redis)
+        ds.set_ssap_result(ssap_res)
         self.assertTrue(ds.redis.get(key), f'redis key {key} has been set')
-        got_ssap_res = ssap.SsapResult.from_redis(
-            ds.redis, id1=ssap_res.prot1, id2=ssap_res.prot2, cath_version=ssap_res.cath_version)
+        got_ssap_res = ds.get_ssap_result(
+            id1=ssap_res.prot1, id2=ssap_res.prot2, cath_version=ssap_res.cath_version)
         self.assertIsInstance(got_ssap_res, ssap.SsapResult,
                               'got SsapResult instance from Redis')
 
-    def test_ssap_sync(self):
+    def test_mongo_datastore(self):
+        ds = self.mongo_ds
+        key = self.expected_ssap_key
+        ssap_res = ssap.SsapResult.from_string(
+            self.expected_ssap_line, cath_version=self.cath_version)
+        ds._collection.delete_one({'_id': key})
+        self.assertFalse(ds._collection.find_one({'_id': key}),
+                         f'redis key {key} is empty')
+        ds.set_ssap_result(ssap_res)
+        self.assertTrue(ds._collection.find_one({'_id': key}),
+                        f'redis key {key} has been set')
+        got_ssap_res = ds.get_ssap_result(
+            id1=ssap_res.prot1, id2=ssap_res.prot2, cath_version=ssap_res.cath_version)
+        self.assertIsInstance(got_ssap_res, ssap.SsapResult,
+                              'got SsapResult instance from Redis')
+
+    def test_ssap_redis_sync(self):
         ds = self.redis_ds
         res = tasks.run_ssap(
-            self.id1, self.id2, cath_version=self.cath_version, datastores=['redis://localhost'])
+            self.id1, self.id2, cath_version=self.cath_version, datastore_dsn='redis://localhost')
 
-    def test_ssap_pairs_sync(self):
+    def test_ssap_redis_pairs_sync(self):
         ds = self.redis_ds
         res = tasks.run_ssap_pairs(
-            pairs=self.ssap_pairs, cath_version=self.cath_version, datastores=['redis://localhost'])
+            pairs=self.ssap_pairs, cath_version=self.cath_version, datastore_dsn='redis://localhost')
+
+    def test_ssap_mongo_sync(self):
+        dsn = f"{self.mongo_ds}"
+        res = tasks.run_ssap(
+            self.id1, self.id2, cath_version=self.cath_version, datastore_dsn=dsn)
+
+    def test_ssap_mongo_pairs_sync(self):
+        dsn = f"{self.mongo_ds}"
+        res = tasks.run_ssap_pairs(
+            pairs=self.ssap_pairs, cath_version=self.cath_version, datastore_dsn=dsn)
 
     # def test_ssap_async(self):
     #     ds = ssap.SsapStorageFactory.get('redis://localhost')
