d3fe2bc01694696735bec0ef9af390b912e78a48
isillitoe <ian@sillit.com>
2020-01-10 21:34:06 +0000

    fix typo

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,8 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.basename(__file__)), '..')
+sys.path.insert(0, 
+    os.path.join(os.path.abspath(os.path.basename(__file__)), '..'))
 
 # deps
 import sphinx_rtd_theme
