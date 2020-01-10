8a2cc6b45829a4df25b5e4a3d748f0a697de2c2e
isillitoe <ian@sillit.com>
2020-01-10 21:06:17 +0000

    fix path setup

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,8 @@
 # core
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath(os.path.basename(__file__), '..'))
 
 # deps
 import sphinx_rtd_theme
