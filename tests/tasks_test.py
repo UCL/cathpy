8a55b2bcf46ae3d9ce3994c096292029fc7b207d
Ian Sillitoe <ian@sillit.com>
2021-06-08 22:16:11 +0100

    tidy up optional tests

--- tests/tasks_test.py
+++ tests/tasks_test.py
@@ -21,6 +21,7 @@ def redis_is_available():
     LOG.info('Testing redis connection...')
     try:
         redis.Redis(db=REDIS_TEST_DATABASE).ping()
+        LOG.info('   ... redis available')
         return True
     except redis.RedisError:
         LOG.info('Failed to connect to redis instance (skipping tests ...)')
@@ -30,7 +31,9 @@ def redis_is_available():
 def mongo_is_available():
     LOG.info('Testing mongodb connection...')
     try:
-        pymongo.MongoClient()
+        client = pymongo.MongoClient(serverSelectionTimeoutMS=1000)
+        client.server_info()
+        LOG.info('   ... mongo available')
         return True
     except Exception as err:
         LOG.info(
@@ -38,18 +41,16 @@ def mongo_is_available():
         return False
 
 
-@pytest.mark.skipif(not redis_is_available(), reason="Failed to connect to local redis instance (skipping tests)")
-@pytest.mark.skipif(not mongo_is_available(), reason="Failed to connect to local mongo instance (skipping tests)")
-class TestSsap(testutils.TestBase):
-
-    def setUp(self):
+class TestObject:
+    def __init__(self):
         self.id1 = '4b0hB00'
         self.id2 = '3hzaA01'
         self.expected_ssap_line = '4b0hB00  3hzaA01  143  127  86.65  114   79   26   1.60'
         self.expected_ssap_key = ssap.SsapResult.mk_key(
             cath_version='current', id1=self.id1, id2=self.id2)
         self.test_data_dir = path.join(path.dirname(__file__), 'data')
-        self.ssap_pairs_file = path.join(self.test_data_dir, 'ssap_pairs.txt')
+        self.ssap_pairs_file = path.join(
+            self.test_data_dir, 'ssap_pairs.txt')
         self.ssap_results_file = path.join(
             self.test_data_dir, 'ssap_results.txt')
         self.cath_version = 'current'
@@ -66,59 +67,105 @@ class TestSsap(testutils.TestBase):
                 pairs.extend([[id1, id2]])
         self.ssap_pairs = pairs
 
-    def test_redis_datastore(self):
-        ds = self.redis_ds
-        key = self.expected_ssap_key
-        ssap_res = ssap.SsapResult.from_string(
-            self.expected_ssap_line, cath_version=self.cath_version)
-        ds.redis.delete(key)
-        self.assertFalse(ds.redis.get(key),
-                         f'redis key {key} is empty')
-        ds.set_ssap_result(ssap_res)
-        self.assertTrue(ds.redis.get(key), f'redis key {key} has been set')
-        got_ssap_res = ds.get_ssap_result(
-            id1=ssap_res.prot1, id2=ssap_res.prot2, cath_version=ssap_res.cath_version)
-        self.assertIsInstance(got_ssap_res, ssap.SsapResult,
-                              'got SsapResult instance from Redis')
 
-    def test_mongo_datastore(self):
-        ds = self.mongo_ds
-        key = self.expected_ssap_key
-        ssap_res = ssap.SsapResult.from_string(
-            self.expected_ssap_line, cath_version=self.cath_version)
-        ds._collection.delete_one({'_id': key})
-        self.assertFalse(ds._collection.find_one({'_id': key}),
-                         f'redis key {key} is empty')
-        ds.set_ssap_result(ssap_res)
-        self.assertTrue(ds._collection.find_one({'_id': key}),
-                        f'redis key {key} has been set')
-        got_ssap_res = ds.get_ssap_result(
-            id1=ssap_res.prot1, id2=ssap_res.prot2, cath_version=ssap_res.cath_version)
-        self.assertIsInstance(got_ssap_res, ssap.SsapResult,
-                              'got SsapResult instance from Redis')
+requires_redis = pytest.mark.skipif(not redis_is_available(),
+                                    reason="Failed to connect to local redis instance (skipping tests)")
+requires_mongo = pytest.mark.skipif(not mongo_is_available(),
+                                    reason="Failed to connect to local mongo instance (skipping tests)")
 
-    def test_ssap_redis_sync(self):
-        ds = self.redis_ds
-        res = tasks.run_ssap(
-            self.id1, self.id2, cath_version=self.cath_version, datastore_dsn='redis://localhost')
 
-    def test_ssap_redis_pairs_sync(self):
-        ds = self.redis_ds
-        res = tasks.run_ssap_pairs(
-            pairs=self.ssap_pairs, cath_version=self.cath_version, datastore_dsn='redis://localhost')
+@pytest.fixture
+def test_object():
+    return TestObject()
 
-    def test_ssap_mongo_sync(self):
-        dsn = f"{self.mongo_ds}"
-        res = tasks.run_ssap(
-            self.id1, self.id2, cath_version=self.cath_version, datastore_dsn=dsn)
 
-    def test_ssap_mongo_pairs_sync(self):
-        dsn = f"{self.mongo_ds}"
-        res = tasks.run_ssap_pairs(
-            pairs=self.ssap_pairs, cath_version=self.cath_version, datastore_dsn=dsn)
+@requires_redis
+@requires_mongo
+def test_redis_datastore(test_object):
+    ds = test_object.redis_ds
+    key = test_object.expected_ssap_key
+    ssap_res = ssap.SsapResult.from_string(
+        test_object.expected_ssap_line,
+        cath_version=test_object.cath_version)
 
-    # def test_ssap_async(self):
-    #     ds = ssap.SsapStorageFactory.get('redis://localhost')
-    #     res = tasks.run_ssap.delay(
-    #         self.id1, self.id2, cath_version=self.cath_version, datastores=[ds])
-    #     res.get(timeout=5)
+    ds.redis.delete(key)
+    assert ds.redis.get(key) is False
+
+    ds.set_ssap_result(ssap_res)
+    assert ds.redis.get(key) is True
+
+    got_ssap_res = ds.get_ssap_result(
+        id1=ssap_res.prot1,
+        id2=ssap_res.prot2,
+        pdb_path=test_object.pdb_path,
+        cath_version=ssap_res.cath_version)
+    assert isinstance(got_ssap_res, ssap.SsapResult)
+
+
+@requires_mongo
+def test_mongo_datastore(test_object):
+    ds = test_object.mongo_ds
+    key = test_object.expected_ssap_key
+    ssap_res = ssap.SsapResult.from_string(
+        test_object.expected_ssap_line,
+        cath_version=test_object.cath_version)
+    ds._collection.delete_one({'_id': key})
+    assert ds._collection.find_one({'_id': key}) is False
+
+    ds.set_ssap_result(ssap_res)
+    assert ds._collection.find_one({'_id': key}) is True
+
+    got_ssap_res = ds.get_ssap_result(
+        id1=ssap_res.prot1,
+        id2=ssap_res.prot2,
+        cath_version=ssap_res.cath_version,
+        pdb_path=test_object.pdb_path,)
+    assert isinstance(got_ssap_res, ssap.SsapResult)
+
+
+@requires_redis
+def test_ssap_redis_sync(test_object):
+    ds = test_object.redis_ds
+    res = tasks.run_ssap(
+        test_object.id1,
+        test_object.id2,
+        cath_version=test_object.cath_version,
+        pdb_path=test_object.pdb_path,
+        datastore_dsn='redis://localhost')
+
+
+@requires_redis
+def test_ssap_redis_pairs_sync(test_object):
+    ds = test_object.redis_ds
+    res = tasks.run_ssap_pairs(
+        pairs=test_object.ssap_pairs,
+        cath_version=test_object.cath_version,
+        pdb_path=test_object.pdb_path,
+        datastore_dsn='redis://localhost')
+
+
+@requires_mongo
+def test_ssap_mongo_sync(test_object):
+    dsn = f"{test_object.mongo_ds}"
+    res = tasks.run_ssap(
+        test_object.id1,
+        test_object.id2,
+        cath_version=test_object.cath_version,
+        pdb_path=test_object.pdb_path,
+        datastore_dsn=dsn)
+
+
+@requires_mongo
+def test_ssap_mongo_pairs_sync(test_object):
+    dsn = f"{test_object.mongo_ds}"
+    res = tasks.run_ssap_pairs(
+        pairs=test_object.ssap_pairs,
+        cath_version=test_object.cath_version,
+        pdb_path=test_object.pdb_path,
+        datastore_dsn=dsn)
+
+# def test_ssap_async(self):
+#     ds = ssap.SsapStorageFactory.get('redis://localhost')
+#     res = tasks.run_ssap.delay(
+#         self.id1, self.id2, cath_version=self.cath_version, datastores=[ds])
+#     res.get(timeout=5)
